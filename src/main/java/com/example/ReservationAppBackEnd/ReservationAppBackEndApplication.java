cb484101d54bae460a704ace3eec40e9e03efb3a
SKrzysztov <122411690+SKrzysztov@users.noreply.github.com>
2023-10-05 20:58:52 +0200

    Commit init

---
+++ src/main/java/com/example/ReservationAppBackEnd/ReservationAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReservationAppBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationAppBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReservationAppBackEnd/ReservationAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReservationAppBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
