8c91949c6a89003a488193be7438bb33269ced55
SKrzysztov <122411690+SKrzysztov@users.noreply.github.com>
2023-11-29 15:12:36 +0100

    Next step

--- src/main/java/com/example/ReservationAppBackEnd/address/domain/Address.java
+++ src/main/java/com/example/ReservationAppBackEnd/address/domain/Address.java
@@ -1,7 +1,6 @@
 package com.example.ReservationAppBackEnd.address.domain;
 
-import com.example.ReservationAppBackEnd.customServiceProvider.domain.CustomServiceProvider;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 import lombok.*;
 
